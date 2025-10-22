73451b3d5e877a47bd0cdbc90eba38460226f804
k-meds <github.com.vaporizer110@passmail.net>
2025-10-22 13:19:18 +1100

    New post: Your Title

---
+++ content/about/_index.md
@@ -0,0 +1,8 @@
+---
+title: "About"
+date: 2025-10-22
+---
+
+# About Me
+
+Tell your story here...
---
+++ dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Starting Hugo development server..."
+hugo server -D --bind 0.0.0.0
